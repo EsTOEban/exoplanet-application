09cdea9944487a794943b5bb5af66b5e69eb2087
Kevin Haynes <kevin.haynes.dev@gmail.com>
2020-05-12 02:38:43 -0400

    Implement REST backend direct queries

--- src/main/java/com/example/exoplanet/config/SpringFoxConfig.java
+++ src/main/java/com/example/exoplanet/config/SpringFoxConfig.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SpringFoxConfig {
 
     @Bean
